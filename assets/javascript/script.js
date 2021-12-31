241198a84521a7ee655e413372d6af8836ea844a
unknown <austin.w.dennis22@gmail.com>
2021-12-30 20:45:19 -0600

    added css and HTML

---
+++ assets/javascript/script.js
@@ -0,0 +1,21 @@
+console.log("Hello World");
+console.log(document);
+
+var btn = document.querySelector("#submit-button");
+var  numList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var  lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+var lowerList= lowerLetters.split("");
+var upperList = lowerLetters.toUpperCase().split("")
+console.log(lowerList,upperList);
+var specialChar ="!@#$%^&*".split("");
+console.log(specialChar);
+console.log(numList);
+
+// for (var index = 0; index < lowerLetters.length; index++) {
+//     var element = lowerLetters[index];
+//     console.log(element);
+    
+// }
+console.log(lowerList[0]);
+console.log(lowerList[30]);
+console.log(lowerList[25]);
