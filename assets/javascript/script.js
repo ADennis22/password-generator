b3bd4738299a5cea3ce7035d69b1148b04a0a25b
unknown <austin.w.dennis22@gmail.com>
2022-03-09 19:27:52 -0600

    finished password generator

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,21 +1,60 @@
-console.log("Hello World");
-console.log(document);
+console.log();
+
 
 var btn = document.querySelector("#submit-button");
-var  numList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
-var  lowerLetters = "abcdefghijklmnopqrstuvwxyz";
-var lowerList= lowerLetters.split("");
-var upperList = lowerLetters.toUpperCase().split("")
-console.log(lowerList,upperList);
-var specialChar ="!@#$%^&*".split("");
+var numList = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+var lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+var lowerList = lowerLetters.split("");
+var upperList = lowerLetters.toUpperCase().split("");
+console.log(lowerList, upperList);
+var specialChar = "!@#$%^&*".split("");
 console.log(specialChar);
 console.log(numList);
 
-// for (var index = 0; index < lowerLetters.length; index++) {
-//     var element = lowerLetters[index];
-//     console.log(element);
+
+// 
+
+function clickSubmit() {
+    var isUsingUpperCaseLetters = document.querySelector("#one").checked;
+    var isUsingLowerCaseLetters = document.querySelector("#two").checked;
+    var isUsingNumbers = document.querySelector("#three").checked;
+    var isUsingSpecialCharacters = document.querySelector("#four").checked;
+    var passwordLength = document.querySelector("#quantity").value;
+    console.log(passwordLength);
+    var posChar = []
+
+    if (isUsingUpperCaseLetters) {
+        console.log(upperList)
+        posChar = posChar.concat(upperList);
+        console.log(posChar)
+    };
+
     
-// }
-console.log(lowerList[0]);
-console.log(lowerList[30]);
-console.log(lowerList[25]);
+    if (isUsingLowerCaseLetters) {
+        posChar = posChar.concat(lowerList);
+    };
+    
+    if(isUsingNumbers){
+        posChar = posChar.concat(numList);
+    };
+
+
+    if (isUsingSpecialCharacters) {
+        posChar = posChar.concat(specialChar);
+    };
+    console.log(posChar)
+    var password = []
+    console.log(posChar)
+    for (var index = 0; index <= passwordLength-1; index++) {
+        var randomIndex = Math.floor(Math.random() * posChar.length);
+        var randomChar = posChar[randomIndex];
+        password.push(randomChar);
+    }
+    var passText = document.querySelector("#password-text-area")
+    passText.value = password.join("");
+    console.log(password.join(""))
+// 
+}
+
+
+btn.addEventListener("click", clickSubmit)
